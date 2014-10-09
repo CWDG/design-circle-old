06f05eaa7e3c91d91abf94f62ea0d1ef654b85ed
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2014-10-08 17:12:03 -0700

    Created event schema

---
+++ app/models/event.server.model.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var validateEventName = function(name) {
+	return name.length() > 0;
+};
+
+/**
+ * Event Schema
+ */
+var EventSchema = new Schema({
+	name: {
+		type: String,
+		trim: true,
+		default: 'Event',
+		validate: [validateEventName, 'Please assign an event name.']
+	},
+	description: {
+		type: String,
+		trim: true,
+		default: ''
+	},
+	date: {
+		type: Date
+	},
+	updated: {
+		type: Date
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+});
+
+EventSchema.pre('save', function(next) {
+	next();
+});
+
+mongoose.model('Event', EventSchema);
