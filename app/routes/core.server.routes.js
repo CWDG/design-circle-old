5c00527b97cc5a02dd000d600549601df59be181
Jonathan Arnett <jonarnett90@gmail.com>
2014-10-22 20:07:39 -0400

    Changed root route to be index, not landing

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -3,6 +3,5 @@
 module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core');
-	app.route('/tut').get(core.index);
-	app.route('/').get(core.landing);
+	app.route('/').get(core.index);
 };
