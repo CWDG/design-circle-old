c11601f2e854962d1f671dfaa855e3b0e1e746f5
Jonathan Arnett <jonarnett90@gmail.com>
2014-11-19 10:51:27 -0500

    Changed 'posts' routes and title to 'blog'

--- public/modules/posts/config/posts.client.routes.js
+++ public/modules/posts/config/posts.client.routes.js
@@ -6,19 +6,19 @@ angular.module('posts').config(['$stateProvider',
 		// Posts state routing
 		$stateProvider.
 		state('listPosts', {
-			url: '/posts',
+			url: '/blog',
 			templateUrl: 'modules/posts/views/list-posts.client.view.html'
 		}).
 		state('createPost', {
-			url: '/posts/create',
+			url: '/blog/create',
 			templateUrl: 'modules/posts/views/create-post.client.view.html'
 		}).
 		state('viewPost', {
-			url: '/posts/:postId',
+			url: '/blog/:postId',
 			templateUrl: 'modules/posts/views/view-post.client.view.html'
 		}).
 		state('editPost', {
-			url: '/posts/:postId/edit',
+			url: '/blog/:postId/edit',
 			templateUrl: 'modules/posts/views/edit-post.client.view.html'
 		});
 	}
